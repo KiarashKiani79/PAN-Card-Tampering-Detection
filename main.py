7281acbd637221675d3c4742e7de246266010aec
Kiarash Kiani <kiarash.kiani7997@gmail.com>
2024-04-21 18:23:22 +0330

    Converting the format of  tampered image similar to original image.

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 # import the necessary packages
-import subprocess
+import os
 from skimage.metrics import structural_similarity
 import imutils
 import cv2
 from PIL import Image
 import requests
 
-
-subprocess.run(["mkdir", "pan_card_tampering"])
-subprocess.run(["mkdir", "pan_card_tampering/image"])
+# Create directories for storing images
+os.makedirs("pan_card_tampering", exist_ok=True)
+os.makedirs("pan_card_tampering/image", exist_ok=True)
 
 # Open image and display
 original = Image.open(requests.get('https://www.thestatesman.com/wp-content/uploads/2019/07/pan-card.jpg', stream=True).raw)
-tampered = Image.open(requests.get('https://assets1.cleartax-cdn.com/s/img/20170526124335/Pan4.png', stream=True).raw) 
+tampered = Image.open(requests.get('https://assets1.cleartax-cdn.com/s/img/20170526124335/Pan4.png', stream=True).raw)
 
+# Print image size
+print("Original Image size: " + str(original.size))
+print("Tampered Image size: " + str(tampered.size))
+
+# Print image format
+print("Original Image format: " + original.format)
+print("Tampered Image format: " + tampered.format)
+print("-----------------------------------")
+
+# Resize image
+original = original.resize((250,160))
+print("Resized Origninal Image size: " + str(original.size))
+original.save("pan_card_tampering/image/original.png")
+
+tampered = tampered.resize((250,160))
+print("Resized Tampered Image size: " + str(tampered.size))
+original.save("pan_card_tampering/image/tampered.png")
+print("-----------------------------------")
