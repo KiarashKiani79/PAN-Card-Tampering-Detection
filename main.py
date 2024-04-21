33f5a7c88a81ccd2f905a3e255ffa81e39a3b953
Kiarash Kiani <kiarash.kiani7997@gmail.com>
2024-04-21 18:05:20 +0330

    Open image and display

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # import the necessary packages
+import subprocess
 from skimage.metrics import structural_similarity
 import imutils
 import cv2
 from PIL import Image
 import requests
+
+
+subprocess.run(["mkdir", "pan_card_tampering"])
+subprocess.run(["mkdir", "pan_card_tampering/image"])
+
+# Open image and display
+original = Image.open(requests.get('https://www.thestatesman.com/wp-content/uploads/2019/07/pan-card.jpg', stream=True).raw)
+tampered = Image.open(requests.get('https://assets1.cleartax-cdn.com/s/img/20170526124335/Pan4.png', stream=True).raw) 
 
